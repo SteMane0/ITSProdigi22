a54e72966ba9387f7a2408404d0c5acca35b7e48
Utente04 <Utente04@PC04AULAAR>
2023-03-27 12:42:20 +0200

    Aggiungere i file di progetto.

---
+++ PrimaApp/Program.cs
@@ -0,0 +1,15 @@
+﻿//una variabile inizializzata con ? vuole dire che puo assumere qanche null
+//per esempio int? sulLivelloDelMare;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.Clear();
+        string? nome;
+        int slm = int.Parse("1");
+        Console.WriteLine("Come ti chiami?");
+        nome = Console.ReadLine();
+        Console.WriteLine("Ciao " + nome + " ! "); 
+    }
+}
